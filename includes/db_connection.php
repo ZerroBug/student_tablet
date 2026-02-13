93253a5091ff497c43c776a8c6cbf16b4a103bf7
Eric Anane Ababio <anane2020@gmail.com>
2026-02-13 08:45:21 +0000

    initial

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,17 +1,12 @@
 <?php
-// Database connection
-$host = 'localhost';
-$dbname = 'u421860731_tablet_db';
-$username = 'u421860731_oashs';
-$password = 'Ananeeric@172090';
-
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO(
+        "mysql:host=localhost;dbname=u421860731_tablet_db;charset=utf8mb4",
+        "u421860731_oashs",
+        "Ananeeric@172090"
+    );
+    echo "SUCCESS";
 } catch (PDOException $e) {
-    die("Database Connection Failed: " . $e->getMessage());
+    echo $e->getMessage();
 }
-
-
-// $conn= new mysqli('localhost','u687023860_juass98','Ananeeric172090','u687023860_juass98_db')or die("Could not connect to mysql".mysqli_error($con));
 ?>
