8a9d03a1dc2e1165b699e700859908bf5f5fdff9
Eric Anane Ababio <anane2020@gmail.com>
2026-02-13 09:57:20 +0000

    initial

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -2,8 +2,8 @@
 // Database connection
 $host = 'localhost';
 $dbname = 'u421860731_tablet_db';
-$username = 'root';
-$password = '';
+$username = 'u421860731_oashs';
+$password = 'Ananeeric@172090'; // The password you set for this DB user
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
@@ -11,3 +11,4 @@ try {
 } catch (PDOException $e) {
     die("Database Connection Failed: " . $e->getMessage());
 }
+?>
