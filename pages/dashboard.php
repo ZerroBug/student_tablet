aec7e950327851dee84ecdbd4263c01ed13bedac
Eric Anane Ababio <anane2020@gmail.com>
2026-03-04 06:25:32 +0000

    Updated

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -353,7 +353,10 @@ foreach ($classData as $row) {
         }
     });
     </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> -->
+    <script>
+    < /body>
 
-</body>
-
-</html>
+    <
+    /html>
