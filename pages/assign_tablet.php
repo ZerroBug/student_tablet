c0e9cad299f2eb058ccf7b2caeb34d340050ed6e
Eric Anane Ababio <anane2020@gmail.com>
2026-03-17 06:35:38 +0000

    Updated

--- pages/assign_tablet.php
+++ pages/assign_tablet.php
@@ -410,7 +410,7 @@ $assigned_tablets = $assigned_stmt->fetchAll(PDO::FETCH_ASSOC);
             return;
         }
 
-        window.location.href = "../pages/php/export_tablets_csv.php?class_id=" + classId;
+        window.location.href = "pages/php/export_tablets_csv.php?class_id=" + classId;
     }
     </script>
 
