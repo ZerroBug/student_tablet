55bd02ef55ebeed91b3da17cf3100396efb3f757
Eric Anane Ababio <anane2020@gmail.com>
2026-08-05 00:59:18 -0500

    Updated

--- pages/report_tablet.php
+++ pages/report_tablet.php
@@ -110,7 +110,7 @@ foreach ($assignments as $a) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Return / Seize Tablet | Dashboard</title>
+    <title>Report Tablet Issue | Dashboard</title>
 
     <link rel="icon" href="assets/images/logo.jpg" type="image/jpeg">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
